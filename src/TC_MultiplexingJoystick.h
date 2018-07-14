35c75f22c93bf73bb5a03555eceb5bda0e7a49cb
Tyson Lloyd Thwaites <tysonlt@gmail.com>
2018-07-15 02:52:37 +1000

    Mux version that switches channels for each x/y read

---
+++ src/TC_MultiplexingJoystick.h
@@ -0,0 +1,41 @@
+#ifndef THWAITES_CONTROLS_MULTIPLEXING_JOYSTICK_H
+#define THWAITES_CONTROLS_MULTIPLEXING_JOYSTICK_H
+
+#include "Arduino.h"
+#include "TC_Joystick.h"
+
+/**
+ * Extension of joystick to work with a multiplexer.
+ */
+class MultiplexingJoystick : public Joystick {
+
+    public:
+
+        /**
+         * Mux version.
+         */ 
+        MultiplexingJoystick(byte signalPin, Multiplexer *mux, byte muxChannelX, byte muxChannelY, int threshold = 150) : 
+            Joystick(0, 0, threshold), _channelX(muxChannelX), _channelY(muxChannelY) {        
+                _pinX = signalPin; //use x as the sig pin
+                setMultiplexer(mux, 0); //mux channel will be set for each axis at read time
+        }
+
+    protected:
+        byte _channelX, _channelY;
+
+        /**
+         * Switch mux channel before reading each pin.
+         */ 
+        void _readPins() override {
+            
+            _muxChannel = _channelX;
+            _x = analogRead(_pinX);
+
+            _muxChannel = _channelY;
+            _y = analogRead(_pinX);
+
+        }
+
+};
+
+#endif
